5656b2e45f6b455b86e21778d5a5e8ea7636549b
min-gui <msj349911@gmail.com>
2022-08-26 11:31:36 +0900

    connect jpa h2 db

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+}

ad80c92f1ea1fad37b2d6cd079362a69fdd71e75
min-gui <msj349911@gmail.com>
2022-08-29 23:19:18 +0900

    mapping test

--- src/main/java/hellojpa/jpashop/JpaMain.java
+++ src/main/java/hellojpa/jpashop/JpaMain.java
@@ -1,11 +1,13 @@
 package hellojpa.jpashop;
 
 import hellojpa.jpashop.domain.Member;
+import hellojpa.jpashop.domain.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,6 +20,31 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            System.out.println("----------");
+
+            Member member1 = new Member();
+            member1.setName("min");
+            //member1.changeTeam(team); // 단방향 연관관계 설정, 참조 저장
+            //team.getMembers().add(member1);
+
+            team.addMember(member1);
+            em.persist(member1);
+
+
+            //em.flush();
+            //em.clear();
+            System.out.println("-----------");
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+            for (Member m : members){
+                System.out.println("member1.getName() = " + m.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/jpashop/domain/Member.java
+++ src/main/java/hellojpa/jpashop/domain/Member.java
@@ -4,21 +4,28 @@ package hellojpa.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Getter
+@Getter @Setter
 public class Member {
 
     @Id
     @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @Column(name = "username")
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    @Column(name = "team_id")
+//    private Long teamId;
+
+//    private String city;
+//    private String street;
+//    private String zipcode;
 }
